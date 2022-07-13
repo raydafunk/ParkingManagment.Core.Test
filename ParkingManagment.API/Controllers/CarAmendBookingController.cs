55ba2d6754d07ab1ec1986f389de2f3bff6913b0
Ray <r.brown-amory@sky.com>
2022-07-13 07:46:16 +0100

    Add files via upload

---
+++ ParkingManagment.API/Controllers/CarAmendBookingController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkingManagment.Core.Enities;
+using ParkingManagment.Core.Processors;
+using System.Net;
+
+namespace ParkingManagment.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarAmendBookingController : ControllerBase
+    {
+        private readonly ICarParkingAmendBookingProcessor _processor;
+        private readonly ILogger<CarAmendBookingController> _looger;
+        public CarAmendBookingController(ICarParkingAmendBookingProcessor processor, ILogger<CarAmendBookingController> looger)
+        {
+            _processor = processor;
+            _looger = looger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Appointment), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Appointment>> CreateProduct([FromBody] Appointment appointment)
+        {
+            await _processor.CreateProduct(appointment);
+
+            return CreatedAtRoute("GetProduct", new { id = appointment.ParkingPlaceId }, appointment);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Appointment), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Appointment>> UpdateProduct([FromBody] Appointment appointment)
+        {
+            return Ok(await _processor.UpdateProduct(appointment));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Appointment), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Appointment>> DeleteProductById(string id)
+        {
+            return Ok(await _processor.DeleteProduct(id));
+        }
+    }
+}
---
+++ ParkingManagment.API/Controllers/CarBookingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingManagment.Core.Models;
+using ParkingManagment.Core.Processors;
+
+namespace ParkingManagment.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarBookingController : ControllerBase
+    {
+        private ICarParkingRequestProcessor _parkingRequestProcessor;
+
+        public CarBookingController(ICarParkingRequestProcessor parkingRequestProcessor)
+        {
+            this._parkingRequestProcessor = parkingRequestProcessor;
+        }
+        [HttpPost]
+        public async Task<IActionResult> BookCarSpace(CarParkingBookingRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _parkingRequestProcessor.ProcessRequest(request);
+                if (result.Flag == Core.Enums.BookingResultFlag.Success)
+                {
+                    return Ok(result);
+                }
+
+                ModelState.AddModelError(nameof(CarParkingBookingRequest.BookingDateFrom), "No rooms available for given date");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+
+    }
+}
